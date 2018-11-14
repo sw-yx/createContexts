c8c76b8b2feddf0c5a722cb5af5390dd914c6513
swyx <swyx@dontemail.me@>
2018-11-14 15:50:04 -0800

    no jsx

--- example/src/createContexts.js
+++ example/src/createContexts.js
@@ -6,34 +6,67 @@ export default function createContexts(numContexts) {
     .fill(React.createContext)
     .map(x => x());
   function Provider({ children, values }) {
-    return (
-      <InternalProvider
-        values={values}
-        children={children}
-        contexts={contexts}
-      />
-    );
+    return React.createElement(InternalProvider, {
+      values: values,
+      children: children,
+      contexts: contexts
+    });
   }
   return [Provider, ...contexts];
 }
 
 function InternalProvider(props) {
-  console.log({ props });
   const { contexts, values, children } = props;
   const [MyContext, ...remainingContexts] = contexts;
   const [MyValue, ...remainingValues] = values;
   const isLastContext = !remainingContexts.length;
-  return (
-    <MyContext.Provider value={MyValue}>
-      {isLastContext ? (
-        props.children
-      ) : (
-        <InternalProvider
-          contexts={remainingContexts}
-          values={remainingValues}
-          children={props.children}
-        />
-      )}
-    </MyContext.Provider>
+  return React.createElement(
+    MyContext.Provider,
+    { value: MyValue },
+    isLastContext
+      ? props.children
+      : React.createElement(InternalProvider, {
+          contexts: remainingContexts,
+          values: remainingValues,
+          children: props.children
+        })
   );
 }
+// import React from 'react';
+
+// export default function createContexts(numContexts) {
+//   if (!numContexts) throw new Error('pls specify how many contexts');
+//   const contexts = Array(numContexts)
+//     .fill(React.createContext)
+//     .map(x => x());
+//   function Provider({ children, values }) {
+//     return (
+//       <InternalProvider
+//         values={values}
+//         children={children}
+//         contexts={contexts}
+//       />
+//     );
+//   }
+//   return [Provider, ...contexts];
+// }
+
+// function InternalProvider(props) {
+//   const { contexts, values, children } = props;
+//   const [MyContext, ...remainingContexts] = contexts;
+//   const [MyValue, ...remainingValues] = values;
+//   const isLastContext = !remainingContexts.length;
+//   return (
+//     <MyContext.Provider value={MyValue}>
+//       {isLastContext ? (
+//         props.children
+//       ) : (
+//         <InternalProvider
+//           contexts={remainingContexts}
+//           values={remainingValues}
+//           children={props.children}
+//         />
+//       )}
+//     </MyContext.Provider>
+//   );
+// }
